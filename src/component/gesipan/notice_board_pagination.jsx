6fd5b71851be7342f56e6b1f79034bb6cf8b5174
ZeRaGlory <kjkj5757@naver.com>
2024-11-05 10:11:49 +0900

    deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/component/gesipan/notice_board_pagination.css 	new file:   src/component/gesipan/notice_board_pagination.jsx 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/component/gesipan/notice_board_pagination.jsx
@@ -0,0 +1,18 @@
+import './notice_board_pagination.css'
+
+export default function Pagination() {
+  return(
+    // a href달기
+    <nav aria-label="게시판 페이지네이션">
+      <ul className="pagination">
+        <li className="page-item">&laquo;</li>
+        <li className="page-item">1</li>
+        <li className="page-item">2</li>
+        <li className="page-item">3</li>
+        <li className="page-item">4</li>
+        <li className="page-item">5</li>
+        <li className="page-item">&raquo;</li>
+      </ul>
+    </nav>
+  )
+};
