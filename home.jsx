d12aee72134dde17065979439fbd8cb8d6157f21
Jeong-Jae-yun <65651241+Jeong-Jae-yun@users.noreply.github.com>
2024-11-06 14:00:24 +0900

    Add files via upload

---
+++ home.jsx
@@ -0,0 +1,15 @@
+import "./home.css";
+export default function Home() {
+    return (
+      <div className="home">
+        <div className="logo">로고</div>
+        <div className="top">
+        <div className="avatar">아바타 이미지</div>
+        <div className="status-message">상태 메시지</div>
+        </div>
+        <div className="friend">친구창</div>
+      </div>
+      
+  
+    )
+  };
